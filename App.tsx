8c2e4fde5a650d3e62e599ff4af3240ee03db5cc
Jonathan Santos <jonathan.santosaugusto@gmail.com>
2020-08-07 11:16:25 -0300

    Styled page Landing, created @types/index.tsx in src and installed packages expo-font, @expo-google-fonts/archivo and @expo-google-fonts/poppins

--- App.tsx
+++ App.tsx
@@ -1,12 +1,27 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
+import { AppLoading } from 'expo'
+
+import { Archivo_400Regular, Archivo_700Bold, useFonts } from '@expo-google-fonts/archivo'
+import { Poppins_400Regular, Poppins_600SemiBold } from '@expo-google-fonts/poppins'
 
 import Landing from './src/pages/landing';
 
 const App = () => {
+  let [fontsLoaded] = useFonts({
+    Archivo_400Regular,
+    Archivo_700Bold,
+    Poppins_400Regular,
+    Poppins_600SemiBold
+  })
+
+  if (!fontsLoaded) {
+    return <AppLoading />
+  }
+  
   return (
     <>
-      <StatusBar />
+      <StatusBar style='light' />
       <Landing />
     </>
   );
--- src/pages/landing/index.tsx
+++ src/pages/landing/index.tsx
@@ -1,12 +1,41 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, Image, TouchableOpacity } from 'react-native'
+
+import landing from '../../assets/images/landing.png'
+import studyIcon from '../../assets/images/icons/study.png'
+import giveClassesIcon from '../../assets/images/icons/give-classes.png'
+import heartIcon from '../../assets/images/icons/heart.png'
 
 import styles from './styles'
 
 const Landing = () => {
 	return (
     <View style={styles.container}>
-			<Text>Oilá</Text>
+			<Image source={ landing } style={styles.banner} />
+
+			<Text style={styles.title}>
+				Seja bem vindo, {'\n'}
+				<Text style={styles.titleBold}>O que deseja fazer?</Text>
+			</Text>
+
+			<View style={styles.buttonsContainer}>
+				<TouchableOpacity style={[styles.button, styles.buttonPrimary]}>
+					<Image source={studyIcon} />
+
+					<Text style={styles.buttonText}>Estudar</Text>
+				</TouchableOpacity>
+
+				<TouchableOpacity style={[styles.button, styles.buttonSecondary]}>
+					<Image source={giveClassesIcon} />
+
+					<Text style={styles.buttonText}>Dar aulas</Text>
+				</TouchableOpacity>
+			</View>
+
+			<Text style={styles.totalConnections}>
+				Total de 285 conexões já realizadas {' '}
+				<Image source={heartIcon} />
+			</Text>
 		</View>
 	)
 }
