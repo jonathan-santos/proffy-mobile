354326034fd7cb6781eed40105dc37c7efdd9f2c
Jonathan Santos <jonathan.santosaugusto@gmail.com>
2020-08-07 12:32:03 -0300

    Created framework for age Favorites, created route component StudyTabs, changed Landing navigation from TeacherList to StudyTabs

--- src/routes/studyTabs.tsx
+++ src/routes/studyTabs.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { createBottomTabNavigator, BottomTabBarOptions } from '@react-navigation/bottom-tabs'
+import { Ionicons } from '@expo/vector-icons'
 
 import TeacherList from '../pages/teacherList'
 import Favorites from '../pages/favorites'
@@ -7,10 +8,52 @@ import Favorites from '../pages/favorites'
 const { Navigator, Screen } = createBottomTabNavigator()
 
 const StudyTabs = () => (
-  <Navigator>
-    <Screen name='TeacherList' component={TeacherList} />
-    <Screen name='Favorites' component={Favorites} />
+  <Navigator tabBarOptions={topBarOptions}>
+    <Screen
+      name='TeacherList'
+      component={TeacherList}
+      options={{
+        tabBarLabel: 'Proffys',
+        tabBarIcon: (props) => <Ionicons name='ios-easel' {...props} />
+      }}
+    />
+
+    <Screen
+      name='Favorites'
+      component={Favorites}
+      options={{
+        tabBarLabel: 'Favoritos',
+        tabBarIcon: (props) => <Ionicons name='ios-heart' {...props} />
+      }}
+    />
   </Navigator>
 )
+
+const topBarOptions: BottomTabBarOptions = {
+  style: {
+    elevation: 0,
+    shadowOpacity: 0,
+    height: 64
+  },
+  tabStyle: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  iconStyle: {
+    flex: 0,
+    width: 20,
+    height: 20
+  },
+  labelStyle: {
+    fontFamily: 'Archivo_700Bold',
+    fontSize: 13,
+    marginLeft: 16
+  },
+  inactiveBackgroundColor: '#FAFCFC',
+  activeBackgroundColor: '#EBEBF5',
+  inactiveTintColor: '#C1BCCC',
+  activeTintColor: '#32264D'
+}
 
 export default StudyTabs
