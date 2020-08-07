7b3f2c6f0a95c2934b65ded648480c937b177460
Jonathan Santos <jonathan.santosaugusto@gmail.com>
2020-08-07 12:01:08 -0300

    Styled page GiveClasses

--- src/pages/giveClasses/index.tsx
+++ src/pages/giveClasses/index.tsx
@@ -1,11 +1,37 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, ImageBackground, Text } from 'react-native'
+import { RectButton } from 'react-native-gesture-handler'
+import * as Linking from 'expo-linking';
+
+import GiveClassesBgImage from '../../assets/images/give-classes-background.png'
+
 import styles from './styles'
 
 const GiveClasses = () => {
+  const handleNavigateToTeacherFormWebPage = () => {
+    Linking.openURL('https://google.com')
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Give classes</Text>
+      <ImageBackground
+        source={GiveClassesBgImage}
+        resizeMode='contain'
+        style={styles.content}
+      >
+        <Text style={styles.title}>
+          Quer ser {'\n'}
+          um Proffy?
+        </Text>
+        
+        <Text style={styles.description}>
+          Para começar, você precisa se cadastrar como professor na nossa plataforma web.
+        </Text>
+      </ImageBackground>
+
+      <RectButton style={styles.okButton} onPress={handleNavigateToTeacherFormWebPage}>
+        <Text style={styles.okButtonText}>Começar</Text>
+      </RectButton>
     </View>
   )
 }
